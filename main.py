2268d070a03aaa93ad300b95b9bfe2c8d5f1bd4b
Egor Kozlov <egkozlov@nes.ru>
2019-12-08 11:50:42 -0600

    Starting some refactoring

--- main.py
+++ main.py
@@ -23,11 +23,12 @@ if __name__ == '__main__':
   
     
 
-    dc = DivorceCosts(unilateral_divorce=False,assets_kept = 1.0,u_lost_m=0.02,u_lost_f=0.02,eq_split=0.0)
+    dc = DivorceCosts(unilateral_divorce=True,assets_kept = 1.0,u_lost_m=0.05,u_lost_f=0.05,eq_split=0.0)
     sc = DivorceCosts(unilateral_divorce=True,assets_kept = 1.0,u_lost_m=0.00,u_lost_f=0.00)
-    #Solve the model
+    
+    
     mdl = Model(iterator_name='default-timed',
-                divorce_costs=dc,separation_costs=sc)
+                divorce_costs=dc,separation_costs=sc,sigma_psi=0.2)
     
     graphs=True
     #gassets,iexo,state,gtheta=mdl.solve_sim()

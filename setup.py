30f1602dfc126236cbd189afa2d0ec3087281c49
Fabio <fabio.blasutt@uclouvain.be>
2020-01-23 18:17:53 -0600

    two policies in place

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from scipy import sparse
 class ModelSetup(object):
     def __init__(self,nogrid=False,divorce_costs='Default',separation_costs='Default',**kwargs): 
         p = dict()       
-        period_year=6 #this can be 1,2,3 or 6
+        period_year=3#this can be 1,2,3 or 6
         T = int(52/period_year)
         Tret = int(42/period_year) # first period when the agent is retired
         Tbef=int(2/period_year)

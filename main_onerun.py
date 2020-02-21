f2dc7e82fcb7fafd7f64174f7b2b3488c2762d29
Fabio <fabio.blasutt@uclouvain.be>
2020-02-21 07:57:25 -0600

    ready for building it up

--- main_onerun.py
+++ main_onerun.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     #For graphs later
     graphs=True
     #Build  data moments and pickle them
-    dat_moments(period=1)
+    dat_moments(period=6)
     
          
     #Initialize the file with parameters
@@ -67,8 +67,8 @@ if __name__ == '__main__':
     
     out, mdl, agents, res = mdl_resid(x0,return_format=['distance','models','agents','scaled residuals'],
                                       #load_from=['mdl_save_bil.pkl','mdl_save_uni.pkl'],
-                                      solve_transition=True,                                    
-                                      save_to=['mdl_save_bil.pkl','mdl_save_uni.pkl'],
+                                      solve_transition=False,#                                    
+                                      save_to=['mdl_save_bil.pkl'],#'mdl_save_uni.pkl'],
                                       store_path=path,
                                       verbose=True,calibration_report=False,draw=True,graphs=graphs)
                          
